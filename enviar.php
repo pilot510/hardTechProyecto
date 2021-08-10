ce6636f0323c29932d6bea34bf1f39f623b46629
santiago510 <santiagoiannucci38@gmail.com>
2021-08-10 14:19:22 -0300

    formulario terminado y funcionando

--- enviar.php
+++ enviar.php
@@ -1,9 +1,9 @@
 <?php
-$destino = "4life788@protonmail.com"
-$nombre = $_POST["nombre"]
-$apellido = $_POST["apellido"]
-$correo = $_POST["correo"]
-$cometarios = $_POST["comentarios"]
+$destino = "4life788@protonmail.com";
+$nombre = $_POST["nombre"];
+$apellido = $_POST["apellido"];
+$correo = $_POST["correo"];
+$cometarios = $_POST["comentarios"];
 
 $contenido = "Nombre: " . $nombre . "\nApellido: " . $apellido . "\nCorreo: " . $correo . "\nComentarios: " . $cometarios;
 
